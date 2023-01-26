dd509cecfc5278d4cc12cc4a490dc58fbeed40b5
Vivek Gupta <118158473+VivekBGupta@users.noreply.github.com>
2023-01-26 10:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+let i = 0
+	let j = str.length-1;
+	while(j-- > i++){
+		if(str.charAt(i) != str.charAt(j)){
+			return false;
+		}
+	}
+	return true
 }
 module.exports = palindrome
